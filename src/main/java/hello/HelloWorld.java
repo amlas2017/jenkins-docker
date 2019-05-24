dda924b180fc9e7e4204abfeb22912b583bfd4f9
amlas2017 <amlas2010@live.fr>
2019-05-24 10:52:17 +0000

    MAJ classe java....

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,6 +4,7 @@ package hello;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Message message = new Message();
+                System.out.println("... JE SUIS DANS LA CLASSE HelloWorld......");
 		System.out.println(message.sayHello());
 	}
 }
